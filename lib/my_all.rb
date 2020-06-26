aa840c0c300f88e0aa5ffcbe1d5bfee07ec833a7
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-26 03:00:52 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,17 +1,14 @@
-require 'pry'
-
-def my_all?(collection)
-  i=0
-  block_retun_values = []
-while i < collection.length
-block_return_values <<
-yield(collection[i])
-i = i + 1 
-end
-
-if block_return_values.include?(false)
-  false
-else
-  true
-end
+ef my_all?(collection)
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
